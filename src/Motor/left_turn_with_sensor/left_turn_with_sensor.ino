af57b4263cfd636ce4fe233983c449edb90ae09f
Dirk Catpo Risco <dcatpo@hotmail.com>
2023-12-02 16:01:49 -0500

    added code to turn left

--- src/Motor/left_turn_with_sensor/left_turn_with_sensor.ino
+++ src/Motor/left_turn_with_sensor/left_turn_with_sensor.ino
@@ -12,6 +12,10 @@
 #define BTNR 2
 #define BTNL 3
 
+// Sensor pins
+#define trigPin 4
+#define echoPin 5
+
 int stepCount = 0;
 
 void setup() {
@@ -25,9 +29,54 @@ void setup() {
   // Pin mode for button control
   pinMode(BTNR, INPUT);
   pinMode(BTNL, INPUT);
+
+  // For sensors
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
 
 void loop() {
+
+  // Continuously measure the distance
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Measure the duration of the echo pulse
+  long duration = pulseIn(echoPin, HIGH);
+
+  // Calculate distance in centimeters
+  // Speed of sound in air at 20°C is approximately 343 meters per second
+  // Divide by 2 because the sound travels to the object and back
+  float distanceCM = duration * 0.0343 / 2;
+
+  // Print the distance to the serial monitor
+  Serial.print("Distance: ");
+  Serial.print(distanceCM);
+  Serial.println(" cm");
+
+  // checks if measured distance is above 39cm 
+  if(distanceCM > 39){
+    delay(10); 
+    stepCount = 0; // resets step count for turning 
+    digitalWrite(DIR, HIGH); // sets direction to go left 
+    
+    // begin loop to turn left when distance is aboce 39cm
+    while(distanceCM > 39){
+      // if stepCount is less than or equal to 100 turn left
+      if(stepCount <= 400){
+        // do one revolution counterclockwise (LOW)
+        delay(HALFPERIODTIME);
+        digitalWrite(PUL, HIGH);
+        delay(HALFPERIODTIME);
+        digitalWrite(PUL, LOW);
+        stepCount++;
+      }
+    }
+  }
+  // delay between measurements 
+  delay(500);
+
   // Check if button right is pushed
   if (digitalRead(BTNR) == HIGH) {
     delay(10);
