8a7b9719e2c1fd7687dde222eaa5b080aefde228
dirky <catpod1@tcnj.edu>
2024-03-25 23:19:47 -0400

    the nao can now recognize images and say what they are in the vis.py file

---
+++ src/NAO/traj.py
@@ -0,0 +1,200 @@
+import os
+import sys
+import time
+sys.path.append('C:/Users/dirky/AppData/Local/Programs/Python/Python27/Python_SDK/lib')
+import naoqi
+from naoqi import ALProxy
+
+NAO_IP = "192.168.1.107"  # Replace here with your NaoQi's IP address.
+PORT = 9559
+
+"""
+names = list()
+times = list()
+keys = list()
+
+names.append("RShoulderRoll")
+times.append([0.36])
+keys.append([[0.280203, [3, -0.133333, 0], [3, 0, 0]]])
+
+try:
+  motion = ALProxy("ALMotion", NAO_IP, PORT)
+  motion.angleInterpolationBezier(names, times, keys)
+except BaseException as err:
+  print(err)
+"""
+"""
+# Choregraphe bezier export in Python.
+names = list()
+times = list()
+keys = list()
+
+names.append("LElbowRoll")
+times.append([0.16])
+keys.append([[-1.02314, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("LElbowYaw")
+times.append([0.16])
+keys.append([[-1.56779, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("LHand")
+times.append([0.16])
+keys.append([[0.2604, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("LShoulderPitch")
+times.append([0.16])
+keys.append([[1.22716, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("LShoulderRoll")
+times.append([0.16])
+keys.append([[0.079726, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("LWristYaw")
+times.append([0.16])
+keys.append([[-1.53251, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("RElbowRoll")
+times.append([0.16])
+keys.append([[0.83147, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("RElbowYaw")
+times.append([0.16])
+keys.append([[1.17193, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("RHand")
+times.append([0.16])
+keys.append([[0.016, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("RShoulderPitch")
+times.append([0.16])
+keys.append([[1.0447, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("RShoulderRoll")
+times.append([0.16])
+keys.append([[0.367591, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("RWristYaw")
+times.append([0.16])
+keys.append([[1.84076, [3, -0.0666667, 0], [3, 0, 0]]])
+
+try:
+  motion = ALProxy("ALMotion", NAO_IP, PORT)
+  motion.angleInterpolationBezier(names, times, keys)
+except BaseException as err:
+  print(err)
+"""
+
+# Choregraphe bezier export in Python.
+names = list()
+times = list()
+keys = list()
+
+names.append("HeadPitch")
+times.append([0.16])
+keys.append([[0.116542, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("HeadYaw")
+times.append([0.16])
+keys.append([[0.0122299, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("LAnklePitch")
+times.append([0.16])
+keys.append([[-0.131966, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("LAnkleRoll")
+times.append([0.16])
+keys.append([[0.0598679, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("LElbowRoll")
+times.append([0.16])
+keys.append([[-1.02314, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("LElbowYaw")
+times.append([0.16])
+keys.append([[-1.56779, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("LHand")
+times.append([0.16])
+keys.append([[0.2604, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("LHipPitch")
+times.append([0.16])
+keys.append([[-0.751618, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("LHipRoll")
+times.append([0.16])
+keys.append([[-0.0873961, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("LHipYawPitch")
+times.append([0.16])
+keys.append([[-0.22699, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("LKneePitch")
+times.append([0.16])
+keys.append([[1.0983, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("LShoulderPitch")
+times.append([0.16])
+keys.append([[1.22716, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("LShoulderRoll")
+times.append([0.16])
+keys.append([[0.079726, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("LWristYaw")
+times.append([0.16])
+keys.append([[-1.53251, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("RAnklePitch")
+times.append([0.16])
+keys.append([[-0.162562, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("RAnkleRoll")
+times.append([0.16])
+keys.append([[-0.0843279, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("RElbowRoll")
+times.append([0.16])
+keys.append([[0.83147, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("RElbowYaw")
+times.append([0.16])
+keys.append([[1.17193, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("RHand")
+times.append([0.16])
+keys.append([[0.016, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("RHipPitch")
+times.append([0.16])
+keys.append([[-0.796188, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("RHipRoll")
+times.append([0.16])
+keys.append([[0.0844119, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("RHipYawPitch")
+times.append([0.16])
+keys.append([[-0.22699, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("RKneePitch")
+times.append([0.16])
+keys.append([[1.1398, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("RShoulderPitch")
+times.append([0.16])
+keys.append([[1.0447, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("RShoulderRoll")
+times.append([0.16])
+keys.append([[0.367591, [3, -0.0666667, 0], [3, 0, 0]]])
+
+names.append("RWristYaw")
+times.append([0.16])
+keys.append([[1.84076, [3, -0.0666667, 0], [3, 0, 0]]])
+
+try:
+  motion = ALProxy("ALMotion", NAO_IP, PORT)
+  motion.angleInterpolationBezier(names, times, keys)
+except BaseException as err:
+  print(err)
--- src/NAO/vis.py
+++ src/NAO/vis.py
@@ -15,18 +15,24 @@ PC_IP = "192.168.1.149"   # Replace here with your computer IP address
 PORT = 9559
 memValue = "PictureDetected" # ALMemory variable where the ALVisionRecognition module outputs its results.
 
-
-# create python module
 class myModule(ALModule):
   """python class myModule test auto documentation"""
+  
+  def __init__(self, name):
+    ALModule.__init__(self, name)
+    self.last_label = None
 
   def pictureChanged(self, strVarName, value, strMessage):
     """callback when data change"""
-    print("datachanged", strVarName, " ", value, " ", strMessage)
+    print("\ndatachanged", strVarName, " ", value, " ", strMessage)
     global count
     count = count-1
     tts = ALProxy("ALTextToSpeech", NAO_IP, PORT)
-    tts.say("I see a picture")
+    if value:  # Check if value is not empty
+        label = value[1][0][0][0]
+        if label != self.last_label:  # Check if label has changed
+            tts.say("I see a " + label)
+            self.last_label = label  # Update last_label
 
 broker = ALBroker("pythonBroker", PC_IP,9999, NAO_IP,9559)
 pythonModule = myModule(moduleName)
@@ -38,14 +44,12 @@ except RuntimeError,e:
   print("Error when creating ALMemory proxy:")
   exit(1)
 
-
 # Have the python module called back when picture recognition results change.
 try:
   memoryProxy.subscribeToEvent(memValue, moduleName, "pictureChanged")
 except RuntimeError,e:
   print("Error when subscribing to micro event")
   exit(1)
-
 
 # Let the picture recognition run for a little while (will stop after 'count' calls of the callback).
 # You can check the results using a browser connected on your Nao, then
@@ -57,6 +61,5 @@ while count>0:
 # unsubscribe modules
 memoryProxy.unsubscribeToEvent(memValue, moduleName)
 #recoProxy.unsubscribe(moduleName)
-
 
 print('end of vision_recognition python script')
