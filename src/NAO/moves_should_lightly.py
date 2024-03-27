879a5c5e6743401bace51b3f3581f85b6b14f34a
dirky <catpod1@tcnj.edu>
2024-03-27 13:44:40 -0400

    fixed the files

---
+++ src/NAO/moves_should_lightly.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import time
+sys.path.append('C:/Users/dirky/AppData/Local/Programs/Python/Python27/Python_SDK/lib')
+import naoqi
+from naoqi import ALProxy
+
+NAO_IP = "192.168.1.126"  # Replace here with your NaoQi's IP address.
+PORT = 9559
+
+
+names = list()
+times = list()
+keys = list()
+
+names.append("RShoulderRoll")
+times.append([0.36])
+keys.append([[0.280203, [3, -0.133333, 0], [3, 0, 0]]])
+
+try:
+  motion = ALProxy("ALMotion", NAO_IP, PORT)
+  motion.angleInterpolationBezier(names, times, keys)
+except BaseException as err:
+  print(err)
+
--- src/NAO/say_pics.py
+++ src/NAO/say_pics.py
@@ -10,7 +10,7 @@ from naoqi import ALBroker
 count = 1
 period = 500
 moduleName = "pythonModule"
-NAO_IP = "192.168.1.107"  # Replace here with your NaoQi's IP address.
+NAO_IP = "192.168.1.126"  # Replace here with your NaoQi's IP address.
 PC_IP = "192.168.1.149"   # Replace here with your computer IP address
 PORT = 9559
 memValue = "PictureDetected" # ALMemory variable where the ALVisionRecognition module outputs its results.
@@ -40,14 +40,14 @@ pythonModule = myModule(moduleName)
 # Create a proxy to ALMemory
 try:
   memoryProxy = ALProxy("ALMemory", NAO_IP, PORT)
-except RuntimeError,e:
+except RuntimeError as e:
   print("Error when creating ALMemory proxy:")
   exit(1)
 
 # Have the python module called back when picture recognition results change.
 try:
   memoryProxy.subscribeToEvent(memValue, moduleName, "pictureChanged")
-except RuntimeError,e:
+except RuntimeError as e:
   print("Error when subscribing to micro event")
   exit(1)
 
